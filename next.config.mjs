c196d222f114429a966d75d06c0871a07744348c
CodingReaper1 <giorgi.gamertube@gmail.com>
2024-10-16 21:06:41 +0400

    performance optimiziation/33

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    // Allow Next.js to optimize images from specific domains
-    // domains: ["example.com"], // Replace with your image host domain(s)
-    // Optionally set a deviceSizes array for responsive images
-    deviceSizes: [320, 420, 768, 1024, 1200, 1920, 2048, 3840],
-    // Optionally set a imageSizes array for specific image sizes
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 500],
-  },
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     // Allow Next.js to optimize images from specific domains
+//     // domains: ["example.com"], // Replace with your image host domain(s)
+//     // Optionally set a deviceSizes array for responsive images
+//     deviceSizes: [320, 420, 768, 1024, 1200, 1920, 2048, 3840],
+//     // Optionally set a imageSizes array for specific image sizes
+//     imageSizes: [16, 32, 48, 64, 96, 128, 256, 500],
+//   },
+// };
 
-export default nextConfig;
+// export default nextConfig;
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+const nextConfig = {};
+
+/** @type {import('next').NextConfig} */
+const config = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+})(nextConfig);
+
+export default config;
