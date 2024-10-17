142adbeeba55cd8b2b4476eb8e98170d5eccfa28
CodingReaper1 <giorgi.gamertube@gmail.com>
2024-10-17 14:06:43 +0400

    performance optimiziation/39

--- app/_components/SectionSubText.tsx
+++ app/_components/SectionSubText.tsx
@@ -1,8 +1,8 @@
 function SectionSubText({ children }: { children: React.ReactNode }) {
   return (
-    <p className="pb-2 text-xl font-medium dark:text-[#dfd9ff] xs:text-[20px] sm:text-[26px] md:pb-5 lg:text-xl lg:leading-[40px]">
+    <h2 className="pb-2 text-xl font-medium dark:text-[#dfd9ff] xs:text-[20px] sm:text-[26px] md:pb-5 lg:text-xl lg:leading-[40px]">
       {children}
-    </p>
+    </h2>
   );
 }
 export default SectionSubText;
