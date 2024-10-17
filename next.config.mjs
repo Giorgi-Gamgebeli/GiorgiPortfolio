b21e1b86488e56dce9a56801789ee9710242454c
CodingReaper1 <giorgi.gamertube@gmail.com>
2024-10-17 14:10:26 +0400

    performance optimiziation/40

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // Allow Next.js to optimize images from specific domains
-    // domains: ["example.com"], // Replace with your image host domain(s)
-    // Optionally set a deviceSizes array for responsive images
     deviceSizes: [320, 420, 768, 1024, 1200, 1920, 2048, 3840],
-    // Optionally set a imageSizes array for specific image sizes
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 500],
+  },
+
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.devtool = "source-map";
+    }
+    return config;
   },
 };
 
 export default nextConfig;
+
 // import withBundleAnalyzer from "@next/bundle-analyzer";
 
 // const nextConfig = {};
