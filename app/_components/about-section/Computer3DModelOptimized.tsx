4063b51d30838fb4b46f55225a8e5e1d3676e03c
CodingReaper1 <giorgi.gamertube@gmail.com>
2024-10-18 23:20:20 +0400

    performance optimiziation/54

--- app/_components/about-section/Computer3DModelOptimized.tsx
+++ app/_components/about-section/Computer3DModelOptimized.tsx
@@ -17,10 +17,11 @@ import {
   //  useFrame,
   useThree,
 } from "@react-three/fiber";
+import useKTX2Loader from "@/app/_hooks/useKTX2Loader";
 
 export default function Model() {
   const { gl } = useThree();
-  const ktx2Loader = new KTX2Loader().setTranscoderPath(`/transcoder/`);
+  const ktx2Loader = useKTX2Loader();
 
   const { nodes, materials } = useGLTF(
     "/desktopPcOptimized.glb",
--- app/_components/contact-section/Earth.tsx
+++ app/_components/contact-section/Earth.tsx
@@ -1,10 +1,24 @@
 "use client";
 
-
+import useKTX2Loader from "@/app/_hooks/useKTX2Loader";
 import { useGLTF } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
 
 function Earth() {
-  const { scene } = useGLTF("./earthOptimized.glb", true);
+  const { gl } = useThree();
+
+  const ktx2Loader = useKTX2Loader();
+
+  const { scene } = useGLTF(
+    "/desktopPcOptimized1.glb",
+    true,
+    true,
+    (loader) => {
+      loader.setKTX2Loader(ktx2Loader.detectSupport(gl));
+    },
+  );
+
+  // const { scene } = useGLTF("./earthOptimized.glb", true);
 
   return <primitive object={scene} scale={2.5} position-y={0} rotation-y={0} />;
 }
