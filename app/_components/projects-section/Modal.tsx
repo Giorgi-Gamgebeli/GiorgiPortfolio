c0f3bc7fc85b830912decb4807e4632ba944f247
CodingReaper1 <giorgi.gamertube@gmail.com>
2024-10-20 15:50:29 +0400

    performance optimiziation/69

--- app/_components/projects-section/Modal.tsx
+++ app/_components/projects-section/Modal.tsx
@@ -1,135 +1,138 @@
 "use client";
 
-import {
-  cloneElement,
-  createContext,
-  Dispatch,
-  SetStateAction,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
-import { Icon } from "@iconify/react/dist/iconify.js";
 import MotionComponent from "../MotionComponent";
+import { useEffect, useState } from "react";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import Link from "next/link";
+import FlexBox from "../FlexBox";
+import Image from "next/image";
+import { projects } from "@/app/_utils/constants";
 
-type ModalContextTypes = {
-  openName: string;
-  close: () => void;
-  open: Dispatch<SetStateAction<string>>;
-};
-
-type WindowTypes = {
+type ModalTypes = Pick<
+  (typeof projects)[number],
+  "deploymentServiceImg" | "deployedWebLink" | "sourceCodeLink"
+> & {
   children: React.ReactNode;
-  name: string;
 };
 
-type OpenTypes = {
-  children: React.ReactNode;
-  opens: string;
-};
-
-const ModalContext = createContext<ModalContextTypes | null>(null);
-
-function Modal({ children }: { children: React.ReactNode }) {
-  const [openName, setOpenName] = useState("");
-
-  const close = () => setOpenName("");
-  const open = setOpenName;
-
-  return (
-    <ModalContext.Provider value={{ openName, close, open }}>
-      {children}
-    </ModalContext.Provider>
-  );
-}
-
-function useModalContext() {
-  const context = useContext(ModalContext);
-
-  if (!context) {
-    throw new Error("Modal must be used within a ModalProvider");
-  }
-
-  return context;
-}
-
-function Open({ children, opens: opensWindowName }: OpenTypes) {
-  const { open } = useModalContext();
-
-  return cloneElement(children as React.ReactElement, {
-    onClick: () => open(opensWindowName),
-  });
-}
-
-function Window({ children, name }: WindowTypes) {
-  const { openName, close } = useModalContext();
-
+function Modal({
+  children,
+  deploymentServiceImg,
+  deployedWebLink,
+  sourceCodeLink,
+}: ModalTypes) {
+  const [isOpen, setIsOpen] = useState(false);
   const [timer, setTimer] = useState(10);
+  const isServiceNetlify = `${deploymentServiceImg.src}`.includes("netlify");
+  const buttonTransition = {
+    delay: 0.5,
+    mass: 1,
+    damping: 12,
+    type: "spring",
+    stiffness: 100,
+  } as const;
 
   useEffect(() => {
-    if (name !== openName || timer !== 10) return;
+    if (timer !== 10) return;
     console.log(timer);
     const timeout = setTimeout(() => {
-      close();
-      // setTimer(10);
+      setIsOpen(false);
     }, timer * 1000);
 
     return () => clearTimeout(timeout);
-  }, [name, openName, timer, close]);
-
-  if (name !== openName) return null;
+  }, [isOpen, timer]);
 
   return (
-    <div className="absolute left-0 top-0 z-10 h-full w-full bg-[rgba(229,231,235,0.3)] backdrop-blur-md dark:bg-[rgba(21,16,48,0.3)]">
-      <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 gap-5">
-        {children}
+    <>
+      <div onClick={() => setIsOpen(true)}>{children}</div>
 
-        <MotionComponent
-          initial={{
-            x: 600,
-          }}
-          animate={{
-            x: 0,
-          }}
-          transition={{
-            delay: 0.5,
-            mass: 1,
-            damping: 12,
-            type: "spring",
-            stiffness: 100,
-          }}
-        >
-          <MotionComponent
-            as="button"
-            onClick={() => {
-              close();
-              setTimer(10);
-            }}
-            className="-z-20 flex h-14 w-14 items-center justify-center rounded-full"
-            initial={{
-              background: "conic-gradient(#e53e3e 100%, transparent 0)",
-            }}
-            animate={{
-              background: `conic-gradient(#e53e3e 0%, transparent 0)`,
-            }}
-            transition={{ duration: timer }}
-          >
+      {isOpen && (
+        <div className="absolute left-0 top-0 z-10 h-full w-full bg-[rgba(229,231,235,0.3)] backdrop-blur-md dark:bg-[rgba(21,16,48,0.3)]">
+          <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 gap-5">
+            <FlexBox className="gap-5">
+              <MotionComponent
+                initial={{ x: -600 }}
+                animate={{ x: 0 }}
+                transition={buttonTransition}
+              >
+                <Link
+                  target="_blank"
+                  className="flex h-14 w-14 cursor-pointer items-center justify-center overflow-hidden rounded-full bg-white transition hover:shadow-lg dark:bg-black"
+                  href={sourceCodeLink}
+                >
+                  <Icon
+                    icon={"hugeicons:github"}
+                    className="text-2xl dark:text-xl dark:text-white"
+                  />
+                </Link>
+              </MotionComponent>
+
+              <MotionComponent
+                initial={{ y: -600 }}
+                animate={{ y: 0 }}
+                transition={buttonTransition}
+              >
+                <Link
+                  target="_blank"
+                  className="relative flex size-14 cursor-pointer items-center justify-center overflow-hidden rounded-full bg-white transition hover:shadow-lg dark:bg-black"
+                  href={deployedWebLink}
+                >
+                  <Image
+                    src={deploymentServiceImg}
+                    alt="Website deployment service logo"
+                    className="-z-10 h-full w-full object-contain dark:z-10"
+                    quality={95}
+                  />
+
+                  <Icon
+                    icon={
+                      isServiceNetlify ? `devicon:netlify` : "logos:vercel-icon"
+                    }
+                    className={`absolute left-1/2 top-1/2 block -translate-x-1/2 -translate-y-1/2 dark:hidden ${isServiceNetlify ? "size-10" : "size-6"}`}
+                  />
+                </Link>
+              </MotionComponent>
+            </FlexBox>
+
             <MotionComponent
-              as="span"
-              className="absolute h-12 w-12 rounded-full bg-white dark:bg-black"
-            />
-            <Icon
-              icon="ep:close-bold"
-              className="z-10 text-2xl dark:text-white"
-            />
-          </MotionComponent>
-        </MotionComponent>
-      </div>
-    </div>
+              initial={{
+                x: 600,
+              }}
+              animate={{
+                x: 0,
+              }}
+              transition={buttonTransition}
+            >
+              <MotionComponent
+                as="button"
+                onClick={() => {
+                  setIsOpen(false);
+                  setTimer(10);
+                }}
+                className="-z-20 flex h-14 w-14 items-center justify-center rounded-full"
+                initial={{
+                  background: "conic-gradient(#e53e3e 100%, transparent 0)",
+                }}
+                animate={{
+                  background: `conic-gradient(#e53e3e 0%, transparent 0)`,
+                }}
+                transition={{ duration: timer }}
+              >
+                <MotionComponent
+                  as="span"
+                  className="absolute h-12 w-12 rounded-full bg-white dark:bg-black"
+                />
+                <Icon
+                  icon="ep:close-bold"
+                  className="z-10 text-2xl dark:text-white"
+                />
+              </MotionComponent>
+            </MotionComponent>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
-
-Modal.Open = Open;
-Modal.Window = Window;
 
 export default Modal;
--- app/_components/projects-section/Project.tsx
+++ app/_components/projects-section/Project.tsx
@@ -1,14 +1,10 @@
-"use client";
+"use server";
 
 import TiltedImage from "./TiltedImage";
-import FlexBox from "../FlexBox";
-import Link from "next/link";
-import Image from "next/image";
-import { Icon } from "@iconify/react/dist/iconify.js";
-import Modal from "./Modal";
 import { slideIn } from "@/app/_utils/motion";
 import MotionComponent from "../MotionComponent";
 import { projects } from "@/app/_utils/constants";
+import Modal from "./Modal";
 
 type ProjectTypes = (typeof projects)[number] & {
   index: number;
@@ -24,8 +20,6 @@ function Project({
   deploymentServiceImg,
   index,
 }: ProjectTypes) {
-  const isServiceNetlify = `${deploymentServiceImg.src}`.includes("netlify");
-
   return (
     <MotionComponent
       as="article"
@@ -54,70 +48,14 @@ function Project({
         </ul>
       </div>
 
-      <Modal>
-        <Modal.Open opens="projectModal">
-          <div className="transition xs:px-6 xs:pt-6 md:absolute md:-right-40 md:top-8 md:w-[32rem] md:p-0 md:group-even:-left-40 md:group-even:right-[inital] md:group-hover:-translate-x-3 md:group-hover:translate-y-3 md:group-hover:-rotate-2 md:group-hover:scale-105 md:group-even:group-hover:translate-x-3 md:group-even:group-hover:translate-y-3 md:group-even:group-hover:rotate-2 lg:w-[38rem] xl:w-[47rem]">
-            <TiltedImage image={image} />
-          </div>
-        </Modal.Open>
-        <Modal.Window name="projectModal">
-          <FlexBox className="gap-5">
-            <MotionComponent
-              initial={{ x: -600 }}
-              animate={{ x: 0 }}
-              transition={{
-                delay: 0.5,
-                mass: 1,
-                damping: 12,
-                type: "spring",
-                stiffness: 100,
-              }}
-            >
-              <Link
-                target="_blank"
-                className="flex h-14 w-14 cursor-pointer items-center justify-center overflow-hidden rounded-full bg-white transition hover:shadow-lg dark:bg-black"
-                href={sourceCodeLink}
-              >
-                <Icon
-                  icon={"hugeicons:github"}
-                  className="text-2xl dark:text-xl dark:text-white"
-                />
-              </Link>
-            </MotionComponent>
-
-            <MotionComponent
-              initial={{ y: -600 }}
-              animate={{ y: 0 }}
-              transition={{
-                delay: 0.5,
-                mass: 1,
-                damping: 12,
-                type: "spring",
-                stiffness: 100,
-              }}
-            >
-              <Link
-                target="_blank"
-                className="relative flex size-14 cursor-pointer items-center justify-center overflow-hidden rounded-full bg-white transition hover:shadow-lg dark:bg-black"
-                href={deployedWebLink}
-              >
-                <Image
-                  src={deploymentServiceImg}
-                  alt="Website deployment service logo"
-                  className="-z-10 h-full w-full object-contain dark:z-10"
-                  quality={95}
-                />
-
-                <Icon
-                  icon={
-                    isServiceNetlify ? `devicon:netlify` : "logos:vercel-icon"
-                  }
-                  className={`absolute left-1/2 top-1/2 block -translate-x-1/2 -translate-y-1/2 dark:hidden ${isServiceNetlify ? "size-10" : "size-6"}`}
-                />
-              </Link>
-            </MotionComponent>
-          </FlexBox>
-        </Modal.Window>
+      <Modal
+        sourceCodeLink={sourceCodeLink}
+        deployedWebLink={deployedWebLink}
+        deploymentServiceImg={deploymentServiceImg}
+      >
+        <div className="transition xs:px-6 xs:pt-6 md:absolute md:-right-40 md:top-8 md:w-[32rem] md:p-0 md:group-even:-left-40 md:group-even:right-[inital] md:group-hover:-translate-x-3 md:group-hover:translate-y-3 md:group-hover:-rotate-2 md:group-hover:scale-105 md:group-even:group-hover:translate-x-3 md:group-even:group-hover:translate-y-3 md:group-even:group-hover:rotate-2 lg:w-[38rem] xl:w-[47rem]">
+          <TiltedImage image={image} />
+        </div>
       </Modal>
     </MotionComponent>
   );
