9db0d38f2ab2b8f14272e1f2acaa9365b2799904
CodingReaper1 <giorgi.gamertube@gmail.com>
2024-10-20 18:14:59 +0400

    performance optimiziation

--- app/_components/contact-section/Earth.tsx
+++ app/_components/contact-section/Earth.tsx
@@ -1,9 +1,16 @@
 "use client";
 
 import { useGLTF } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
+import { KTX2Loader } from "three-stdlib";
 
 function Earth() {
-  const { scene } = useGLTF("./earthOptimized.glb", true);
+  const ktx2Loader = new KTX2Loader().setTranscoderPath(`/transcoder/`);
+  // const { scene } = useGLTF("./earthOptimized.glb", true);
+  const { gl } = useThree();
+  const { scene } = useGLTF("/earthOptimized1.glb", true, true, (loader) =>
+    loader.setKTX2Loader(ktx2Loader.detectSupport(gl)),
+  );
 
   return <primitive object={scene} scale={2.5} position-y={0} rotation-y={0} />;
 }
