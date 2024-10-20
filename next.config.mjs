13f93b3d42a3a742cf8cd351987debdb8c1237d1
CodingReaper1 <giorgi.gamertube@gmail.com>
2024-10-20 16:41:01 +0400

    phone performance optimization

--- next.config.mjs
+++ next.config.mjs
@@ -4,19 +4,32 @@ const nextConfig = {
     deviceSizes: [320, 420, 768, 1024, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 500],
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: "@svgr/webpack",
-        },
-      ],
-    });
-
-    return config;
-  },
 };
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     deviceSizes: [320, 420, 768, 1024, 1200, 1920, 2048, 3840],
+//     imageSizes: [16, 32, 48, 64, 96, 128, 256, 500],
+//   },
+//   webpack(config) {
+//     config.module.rules.push({
+//       test: /\.svg$/,
+//       use: [
+//         {
+//           loader: "@svgr/webpack",
+//           options: {
+//             // You can specify options here if needed
+//             // e.g., icon: true
+//           },
+//         },
+//       ],
+//     });
+
+//     return config;
+//   },
+// };
+
 
 export default nextConfig;
 
