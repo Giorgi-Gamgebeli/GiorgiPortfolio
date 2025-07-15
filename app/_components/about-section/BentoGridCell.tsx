9d6f156a8023e5ed3a0836dd779de25ec4c60e6a
Giorgi <giorgi.gamertube@gmail.com>
2025-07-15 13:35:35 +0400

    prod error

--- app/_components/about-section/BentoGridCell.tsx
+++ app/_components/about-section/BentoGridCell.tsx
@@ -17,7 +17,7 @@ function BentoGridCell({
     <MotionComponent
       as="p"
       id={`${id}`}
-      className="block h-full overflow-hidden hyphens-auto rounded-2xl border border-black/5 border-gray-300 bg-white p-4 font-normal shadow-md dark:border-transparent dark:bg-darkTertiary dark:text-white dark:shadow-none md:text-base md:leading-relaxed md:tracking-wider lg:text-lg"
+      className={`block h-full overflow-hidden hyphens-auto rounded-2xl border border-black/5 border-gray-300 bg-white p-4 font-normal shadow-md dark:border-transparent dark:bg-darkTertiary dark:text-white dark:shadow-none md:text-base md:leading-relaxed md:tracking-wider lg:text-lg ${className}`}
       {...rest}
     >
       {children}
